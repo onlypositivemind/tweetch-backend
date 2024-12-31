1107dcfb71671ab6d333b3bae694dfff23452069
Evgeny Petrov <evgenypetrov5230@gmail.com>
2024-12-31 17:09:02 +0300

    init, added prisma

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core'
+
+import { CoreModule } from './core/core.module'
+
+async function bootstrap() {
+	const app = await NestFactory.create(CoreModule)
+	await app.listen(process.env.PORT ?? 3000)
+}
+
+bootstrap()
